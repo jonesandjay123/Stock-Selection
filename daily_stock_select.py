bd87d7ce603e89d6a4049cb73ede10d4cd105ab2
joneswang <jonescwang@gmail.com>
2023-04-18 19:46:47 -0400

    working version

--- daily_stock_select.py
+++ daily_stock_select.py
@@ -3,8 +3,10 @@ import datetime
 import pandas as pd
 import nasdaqdatalink as ndl
 
+N = 15  # 宣告N的值
+
 # https://github.com/Nasdaq/data-link-python
-def get_top_15_stocks():
+def get_top_N_stocks(N):
     # 設定日期範圍（過去一年）
     end_date = datetime.date.today()
     start_date = end_date - datetime.timedelta(days=365)
@@ -16,9 +18,8 @@ def get_top_15_stocks():
 
     for stock_symbol in stock_list:
         # 獲取股票歷史數據
-        stock_data = ndl.Dataset(f'WIKI/{stock_symbol}').data(params={'start_date': start_date.isoformat(), 'end_date': end_date.isoformat()})
+        stock_data = ndl.get(f'WIKI/{stock_symbol}', start_date=start_date, end_date=end_date)
         stock_df = pd.DataFrame(stock_data)
-        
         # 計算平均收盤價
         average_close = stock_df['Close'].mean()
 
@@ -32,19 +33,19 @@ def get_top_15_stocks():
         # 將結果加入stock_results
         stock_results.append({'symbol': stock_symbol, 'average_close': average_close, 'average_volume': average_volume, 'buy_price': buy_price, 'sell_price': sell_price})
 
-    # 根據平均交易量降序排序，取前15名
-    top_15_stocks = sorted(stock_results, key=lambda x: x['average_volume'], reverse=True)[:15]
+    # 根據平均交易量降序排序，取前N名
+    top_N_stocks = sorted(stock_results, key=lambda x: x['average_volume'], reverse=True)[:N]
 
-    return top_15_stocks
+    return top_N_stocks
 
 def main():
-    top_15_stocks = get_top_15_stocks()
+    top_N_stocks = get_top_N_stocks(N)
 
     # 將結果轉為JSON格式
-    top_15_stocks_json = json.dumps(top_15_stocks, indent=2)
+    top_N_stocks_json = json.dumps(top_N_stocks, indent=2)
 
     # 輸出結果
-    print(top_15_stocks_json)
+    print(top_N_stocks_json)
 
 if __name__ == "__main__":
     main()
