e5a03e5c68d71f21d0297bffe3f09f2cd282d88d
joneswang <jonescwang@gmail.com>
2023-04-18 17:17:12 -0400

    add new version of code

--- daily_stock_select.py
+++ daily_stock_select.py
@@ -1,84 +1,50 @@
-import requests
 import json
-from typing import List
-from indicators.calculate import Calculator
-from tqdm import tqdm
+import datetime
+import pandas as pd
+import nasdaqdatalink as ndl
 
-def read_access_token(file_name):
-    with open(file_name, 'r') as file:
-        return file.read().strip()
+# https://github.com/Nasdaq/data-link-python
+def get_top_15_stocks():
+    # 設定日期範圍（過去一年）
+    end_date = datetime.date.today()
+    start_date = end_date - datetime.timedelta(days=365)
 
-API_KEY = read_access_token('access_token.txt')
+    # 可根據需要自定義股票清單
+    stock_list = ['AAPL', 'GOOGL', 'MSFT', 'AMZN', 'FB', 'TSLA', 'IBM', 'NVDA', 'NFLX', 'BA']
 
-# 修改 get_data 函數以使用 Quandl API
-def get_data(symbol: str) -> dict:
-    start_date = "2021-01-01"
-    end_date = "2021-12-31"
-    url = f"https://data.nasdaq.com/api/datatables/SHARADAR/SF1?ticker={symbol}&dimension=MRY&start_date={start_date}&end_date={end_date}&api_key={API_KEY}"
-    r = requests.get(url)
-    try:
-        data = r.json()
-        if "datatable" not in data:
-            print(f"Failed to get data for {symbol}: datatable key not found")
-            return None
-        return data
-    except json.JSONDecodeError as e:
-        print(f"Failed to get data for {symbol}: {e}")
-        return None
+    stock_results = []
 
-def calculate_indicators(data: dict) -> dict:
-    if data is None:
-        return None
+    for stock_symbol in stock_list:
+        # 獲取股票歷史數據
+        stock_data = ndl.Dataset(f'WIKI/{stock_symbol}').data(params={'start_date': start_date.isoformat(), 'end_date': end_date.isoformat()})
+        stock_df = pd.DataFrame(stock_data)
+        
+        # 計算平均收盤價
+        average_close = stock_df['Close'].mean()
 
-    # 使用新的資料格式提取收盤價
-    close_prices = [row[3] for row in data["datatable"]["data"]]
+        # 計算平均交易量
+        average_volume = stock_df['Volume'].mean()
 
-    # 在這裡計算您需要的技術指標
-    # ...
+        # 計算買入價與賣出價
+        buy_price = average_close * 0.9
+        sell_price = average_close * 1.1
 
-    indicators = {
-        # "RSI": rsi,
-        # "SMA": sma,
-        # ...
-    }
-    return indicators
+        # 將結果加入stock_results
+        stock_results.append({'symbol': stock_symbol, 'average_close': average_close, 'average_volume': average_volume, 'buy_price': buy_price, 'sell_price': sell_price})
 
-def select_stocks(stock_list: List[str]) -> List[dict]:
-    selected_stocks = []
+    # 根據平均交易量降序排序，取前15名
+    top_15_stocks = sorted(stock_results, key=lambda x: x['average_volume'], reverse=True)[:15]
 
-    for stock in tqdm(stock_list):
-        data = get_data(stock)
-        indicators = calculate_indicators(data)
+    return top_15_stocks
 
-        if indicators is None:
-            continue
+def main():
+    top_15_stocks = get_top_15_stocks()
 
-        # 為了簡單起見，這裡只是一個示例，我們假設所有股票都符合策略
-        selected_stocks.append({
-            "symbol": stock,
-            "sell_price": 100,
-            "buy_price": 90
-        })
+    # 將結果轉為JSON格式
+    top_15_stocks_json = json.dumps(top_15_stocks, indent=2)
 
-    # 根據您的策略對 selected_stocks 進行排序，然後返回前 15 名
-    top_stocks = selected_stocks[:15]
-
-    return top_stocks
+    # 輸出結果
+    print(top_15_stocks_json)
 
 if __name__ == "__main__":
-    # with open('voo_stock_list.txt', 'r') as f:
-    #     stock_list = json.load(f)
-    stock_list = ['AAPL', 'MSFT', 'AMZN', 'TSLA', 'NVDA']
-    top_stocks = select_stocks(stock_list)
-
-    # 根據新的資料格式修改結果處理部分
-    results = []
-    for idx, stock in enumerate(top_stocks):
-        result = {
-            "Ranking": idx + 1,
-            "Symbol": stock["symbol"],
-            "Indicators": stock["indicators"]
-        }
-        results.append(result)
-
-    print(json.dumps(results, indent=2))
+    main()
---
+++ daily_stock_select_old.py
@@ -0,0 +1,85 @@
+import requests
+import json
+from typing import List
+from indicators.calculate import Calculator
+from tqdm import tqdm
+
+def read_access_token(file_name):
+    with open(file_name, 'r') as file:
+        return file.read().strip()
+
+API_KEY = read_access_token('access_token.txt')
+
+# 修改 get_data 函數以使用 Nasdaq Data Link API
+def get_data(symbol: str) -> dict:
+    start_date = "2021-01-01"
+    end_date = "2021-12-31"
+    url = f"https://data.nasdaq.com/api/v3/datatables/WIKI/PRICES?ticker={symbol}&date.gte={start_date}&date.lte={end_date}&api_key={API_KEY}"
+    print(url)
+    r = requests.get(url)
+    try:
+        data = r.json()
+        if "datatable" not in data:
+            print(f"Failed to get data for {symbol}: datatable key not found")
+            return None
+        return data
+    except json.JSONDecodeError as e:
+        print(f"Failed to get data for {symbol}: {e}")
+        return None
+
+def calculate_indicators(data: dict) -> dict:
+    if data is None:
+        return None
+
+    # 使用新的資料格式提取收盤價
+    close_prices = [row[11] for row in data["datatable"]["data"]]
+
+    # 在這裡計算您需要的技術指標
+    # ...
+
+    indicators = {
+        # "RSI": rsi,
+        # "SMA": sma,
+        # ...
+    }
+    return indicators
+
+def select_stocks(stock_list: List[str]) -> List[dict]:
+    selected_stocks = []
+
+    for stock in tqdm(stock_list):
+        data = get_data(stock)
+        indicators = calculate_indicators(data)
+
+        if indicators is None:
+            continue
+
+        # 為了簡單起見，這裡只是一個示例，我們假設所有股票都符合策略
+        selected_stocks.append({
+            "symbol": stock,
+            "sell_price": 100,
+            "buy_price": 90
+        })
+
+    # 根據您的策略對 selected_stocks 進行排序，然後返回前 15 名
+    top_stocks = selected_stocks[:15]
+
+    return top_stocks
+
+if __name__ == "__main__":
+    with open('voo_stock_list.txt', 'r') as f:
+        stock_list = json.load(f)
+    stock_list = ['AAPL', 'MSFT', 'AMZN', 'TSLA', 'NVDA']
+    top_stocks = select_stocks(stock_list)
+
+    # 根據新的資料格式修改結果處理部分
+    results = []
+    for idx, stock in enumerate(top_stocks):
+        result = {
+            "Ranking": idx + 1,
+            "Symbol": stock["symbol"],
+            "Price": {"sell_price": stock["sell_price"], "buy_price": stock["buy_price"]}
+        }
+        results.append(result)
+
+    print(json.dumps(results, indent=2))
