cabbcedb90ec200244810d972d4ef3642c0c048a
joneswang <jonescwang@gmail.com>
2023-04-18 20:56:51 -0400

    working version

--- daily_stock_select.py
+++ daily_stock_select.py
@@ -1,38 +1,55 @@
 import json
 import datetime
 import pandas as pd
-import nasdaqdatalink as ndl
+import requests
 
-N = 15  # 宣告N的值
+N = 3  # 宣告N的值
+interval_days = 5  # 宣告日期範圍
 
-# https://github.com/Nasdaq/data-link-python
+def read_access_token(file_name):
+    with open(file_name, 'r') as file:
+        return file.read().strip()
+
+API_KEY = read_access_token('access_token.txt')
+
+def get_stock_data(stock_symbol, start_date, end_date):
+    headers = {'Content-Type': 'application/json'}
+    request_url = f"https://api.tiingo.com/tiingo/daily/{stock_symbol}/prices?startDate={start_date}&endDate={end_date}&token={API_KEY}"
+    response = requests.get(request_url, headers=headers)
+    return response.json()
+
 def get_top_N_stocks(N):
-    # 設定日期範圍（過去一年）
+    # 設定日期範圍（過去interval_days範圍內）
     end_date = datetime.date.today()
-    start_date = end_date - datetime.timedelta(days=10)
+    start_date = end_date - datetime.timedelta(days=interval_days)
 
     # 可根據需要自定義股票清單
-    stock_list = ['AAPL', 'GOOGL', 'MSFT', 'AMZN', 'FB', 'TSLA', 'IBM', 'NVDA', 'NFLX', 'BA']
+    stock_list = ['AAPL', 'MSFT', 'AMZN', 'META', 'TSLA', 'NVDA']
 
     stock_results = []
 
     for stock_symbol in stock_list:
-        # 獲取股票歷史數據
-        stock_data = ndl.get(f'WIKI/{stock_symbol}', start_date=start_date, end_date=end_date)
-        print(stock_data)
-        stock_df = pd.DataFrame(stock_data)
-        # 計算平均收盤價
-        average_close = stock_df['Close'].mean()
+        try:
+            # 獲取股票歷史數據
+            stock_data = get_stock_data(stock_symbol, start_date, end_date)
+            stock_df = pd.DataFrame(stock_data)
 
-        # 計算平均交易量
-        average_volume = stock_df['Volume'].mean()
+            # 計算平均收盤價
+            average_close = stock_df['adjClose'].mean()
 
-        # 計算買入價與賣出價
-        buy_price = average_close * 0.9
-        sell_price = average_close * 1.1
+            # 計算平均交易量
+            average_volume = stock_df['adjVolume'].mean()
 
-        # 將結果加入stock_results
-        stock_results.append({'symbol': stock_symbol, 'average_close': average_close, 'average_volume': average_volume, 'buy_price': buy_price, 'sell_price': sell_price})
+            # 計算買入價與賣出價
+            buy_price = average_close * 0.9
+            sell_price = average_close * 1.1
+
+            # 將結果加入stock_results
+            stock_results.append({'symbol': stock_symbol, 'average_close': average_close, 'average_volume': average_volume, 'buy_price': buy_price, 'sell_price': sell_price})
+        
+        except Exception as e:
+            print(f"Error processing {stock_symbol}: {e}. Skipping this stock.")
+            continue
 
     # 根據平均交易量降序排序，取前N名
     top_N_stocks = sorted(stock_results, key=lambda x: x['average_volume'], reverse=True)[:N]
